78d19fa64e7f5d73cf90c4b6f83a02459a496f75
Cat Meow <110690475+catMeooww@users.noreply.github.com>
2023-11-23 15:32:08 -0300

    Added mobile mode
    Mobile mode is a different version of the game for cellphones (but the computer version has a better experience, so try it in computer to have the best gaming experience this game can show)

--- MobileMode/mobileSketch.js
+++ MobileMode/mobileSketch.js
@@ -9,6 +9,7 @@ const Composite = Matter.Composite;
 
 let engine;
 let world;
+var canvas;
 
 var bg_img;
 var grass;
@@ -25,32 +26,34 @@ var control = {
 }
 
 function preload() {
-    bg_img = loadImage('background.png');
-    grass = loadImage('grass.png');
-    player = loadImage('player.png');
-    goldbar = loadImage("gold.png");
+    bg_img = loadImage('/./background.png');
+    grass = loadImage('/./grass.png');
+    player = loadImage('/./player.png');
+    goldbar = loadImage("/./gold.png");
 }
 
 function setup() {
-
     var celular = /iPhone|Android|iPad/i.test(navigator.userAgent)
-    if(celular){
-        window.location = "MobileMode/mobile.html"
-    }
+    if (celular) {
+        canW = displayWidth;
+        canH = displayHeight;
 
-    canvas = createCanvas(500, 700);
+        canvas = createCanvas(displayWidth, displayHeight);
+    } else {
+        window.location = "/./index.html";
+    }
     canvas.parent(document.getElementById("main"));
     frameRate(80);
     engine = Engine.create();
     world = engine.world;
-    ground = new Ground(250, 670, width, 100);
-    slime = new Player(player, 50, 600, 30, 30);
+    ground = new Ground(210, 500, width, 100);
+    slime = new Player(player, 50, 450, 30, 30);
 
     //plataforms
-    plat1 = new Ground(450, 550, 200, 60);
-    plat2 = new Ground(200, 410, 200, 60);
-    plat3 = new Ground(450, 300, 200, 60);
-    plat4 = new Ground(150, 200, 160, 60);
+    plat1 = new Ground(270, 330, 120, 40);
+    plat2 = new Ground(200, 410, 120, 40);
+    plat3 = new Ground(360, 240, 120, 40);
+    plat4 = new Ground(150, 180, 100, 40);
 
     gold = new Gold(130, 30);
     textSize(50)
@@ -72,16 +75,22 @@ function draw() {
     plat4.show();
 
     gold.show();
+    
     //controls
-    if (keyDown(control.right)) {
-        Matter.Body.translate(slime.body, { x: 5, y: 0 });
-    }
-    if (keyDown(control.left)) {
-        Matter.Body.translate(slime.body, { x: -5, y: 0 });
-    }
-    if (keyDown(control.up)) {
-        jumping();
-    }
+    Jbtn = createImg("arrow_up.png");
+    Jbtn.position(50, height + 100);
+    Jbtn.size(50, 50)
+    Jbtn.mouseClicked(jumping);
+
+    Rbtn = createImg("arrow_right.png");
+    Rbtn.position(75, height + 150);
+    Rbtn.size(50, 50)
+    Rbtn.mouseClicked(righting);
+
+    Lbtn = createImg("arrow_left.png");
+    Lbtn.position(25, height + 150);
+    Lbtn.size(50, 50)
+    Lbtn.mouseClicked(lefting);
 
     //golded
     var golded = Matter.SAT.collides(
@@ -93,29 +102,38 @@ function draw() {
     }
 
 }
+
+//moving
 function jumping() {
     console.log("jump")
     if (inGround) {
         inGround = false;
-        Matter.Body.setVelocity(slime.body, {x:0,y:-10});
+        Matter.Body.setVelocity(slime.body, { x: 0, y: -8 });
         setTimeout(() => {
             inGround = true;
-        }, 1200)
+        }, 1300)
     }
 }
+function righting(){
+    Matter.Body.translate(slime.body, { x: 10, y: 0 });
+}
+function lefting(){
+    Matter.Body.translate(slime.body, { x: -10, y: 0 });
+}
+
 function gameOver() {
     swal(
         {
             title: `Level 1 complete`,
-            text: "Thanks for playing, you are good in this!",
+            text: "Thanks for playing, you are good in this! (better experience in computer version)",
             imageUrl:
-                "gold.png",
+                "/./gold.png",
             imageSize: "150x150",
-            confirmButtonText: "Next Level"
+            confirmButtonText: "Reload"
         },
         function (isConfirm) {
             if (isConfirm) {
-                window.location = "SecondLevel.html";
+                location.reload();
             }
         }
     );
